873cbe1dd4a9bacfe5cf8c75b636c5edf17759d9
JOAO PAULO MEDEIROS ROCHA <1-2023125822>
2024-04-18 08:35:02 -0300

    Adicionando informações para criação do banco de dados

---
+++ database/CreateDatabase.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE enadeSys;
+
+CREATE TABLE exam(
+    id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    question VARCHAR(10000) NOT NULL,
+    option1 VARCHAR(10),
+    option2 VARCHAR(10),
+    option3 VARCHAR(10),
+    option4 VARCHAR(10),
+    option5 VARCHAR(10),
+    answer VARCHAR(10));
