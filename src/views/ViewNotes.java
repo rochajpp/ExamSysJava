b69aa35e98fd24c975995d419f8dc6eff2fad74d
João Paulo <joao.rochap03@gmail.com>
2024-05-03 19:11:36 -0300

    Fazendo Label de cada prova e suas respectivas notas em ViewNotes

--- src/views/ViewNotes.java
+++ src/views/ViewNotes.java
@@ -1,15 +1,17 @@
 package src.views;
 import src.models.*;
-
+import src.Database;
 import java.awt.Font;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
+import java.util.Map;
 
 import javax.swing.*;
 
 public class ViewNotes extends JFrame{
     private Student student;
+    private Database db = new Database();
     
     private JLabel title;
 
@@ -33,6 +35,11 @@ public class ViewNotes extends JFrame{
         title = new JLabel("Provas realizadas:");
         title.setFont(new Font("Segoe UI", Font.BOLD, 20));
         panel.add(title, gbc);
+
+        for (Map.Entry<Integer, Float> entry : student.getExams().entrySet()) {
+            Exam exam = db.getExam(entry.getKey());
+            JLabel examLabel = new JLabel(exam.getId() + " - " + exam.getName() + " Professor: " + exam.getProfessor().getName() + " Nota: " + entry.getValue());
+        }
 
         add(panel);
         setVisible(true);
